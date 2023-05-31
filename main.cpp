f50604ebf0836ec8940790bd184b833dfed71053
Pedro Kunz <pedrokunz094@gmail.com>
2023-05-31 20:21:01 -0300

    Dividindo os menus em funções

--- main.cpp
+++ main.cpp
@@ -1,43 +1,8 @@
 #include <iostream>
 #include <windows.h>
+#include "view/ViewMenuPrincipal.cpp"
 
 using namespace std;
-
-void menuPrincipal(){
-    unsigned short resposta;
-
-    cout <<"\n/////////////////////////////////// Bem-vindo(a) a pizzaria LaPizza /////////////////////////////////////\n";
-
-    cout <<"\n1- Novo pedido  |  2- Pedidos em processamento  |  3- Histórico de pedidos  |  4- Estoque\n";
-    cout <<"\n* Qual atividade você deseja realizar? ";
-    cin >> resposta;
-
-    //Validação da resposta
-    while(resposta != 1 && resposta != 2 && resposta != 3 && resposta != 4){
-        cout <<"\nOpção inválida, por favor tente novamente: ";
-        cin >> resposta;
-    }
-
-    switch (resposta)
-    {
-    case 1:
-        cout << "Novo pedido";
-        break;
-
-    case 2:
-        cout << "Pedidos em processamento"; 
-        break;
-
-    case 3: 
-        cout << "Histórico de pedidos";
-        break;
-
-    case 4: 
-        cout << "Estoque";
-        break;
-    
-    }
-};
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
---
+++ view/ViewMenuPrincipal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "ViewNovoPedido.cpp"
+
+using namespace std;
+
+void menuPrincipal(){
+    unsigned short resposta;
+
+    cout <<"\n/////////////////////////////////// Bem-vindo(a) a pizzaria LaPizza /////////////////////////////////////\n";
+
+    cout <<"\n1- Novo pedido  |  2- Pedidos em processamento  |  3- Histórico de pedidos  |  4- Estoque\n";
+    cout <<"\n* Qual atividade você deseja realizar? ";
+    cin >> resposta;
+
+    //Validação da resposta
+    while(resposta != 1 && resposta != 2 && resposta != 3 && resposta != 4){
+        cout <<"\nOpção inválida, por favor tente novamente: ";
+        cin >> resposta;
+    }
+
+    switch (resposta)
+    {
+    case 1:
+        // View novo pedido
+        novoPedido();
+        break;
+
+    case 2:
+        cout << "Pedidos em processamento"; 
+        break;
+
+    case 3: 
+        cout << "Histórico de pedidos";
+        break;
+
+    case 4: 
+        cout << "Estoque";
+        break;
+    
+    }
+};
